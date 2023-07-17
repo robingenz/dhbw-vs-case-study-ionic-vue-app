2f7f45b234d8f22552da8a18e1c736e46091b85e
Robin Genz <mail@robingenz.dev>
2023-07-17 12:18:52 +0200

    Add implementation

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,80 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Battery</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content>
+      <ion-item>
+        <ion-input
+          label="Battery Level"
+          readonly
+          v-model="batteryLevelAsText"
+        ></ion-input>
+      </ion-item>
+      <ion-button
+        @click="refreshBatteryLevel()"
+        expand="block"
+        :disabled="disableButton"
+        >Refresh</ion-button
+      >
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonItem,
+  IonInput,
+  IonButton,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { Device } from "@capacitor/device";
+
+export default defineComponent({
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonItem,
+    IonInput,
+    IonButton,
+  },
+  data() {
+    return {
+      batteryLevel: undefined as number | undefined,
+      disableButton: false,
+    };
+  },
+  computed: {
+    batteryLevelAsText() {
+      if (this.batteryLevel === undefined) {
+        return "Unknown";
+      } else {
+        return this.batteryLevel * 100 + "%";
+      }
+    },
+  },
+  methods: {
+    async refreshBatteryLevel() {
+      this.disableButton = true;
+      const { batteryLevel } = await Device.getBatteryInfo();
+      this.batteryLevel = batteryLevel;
+      this.disableButton = false;
+    },
+  },
+});
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
